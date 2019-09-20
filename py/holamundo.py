f244f8c935fc663372455674d861dca67ee58537
Peter Rabbit <foo@nowhere.com>
2019-09-20 11:27:07 -0400

    Day 5 playbooks

---
+++ py/holamundo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/phython3
+
+with open('hellosoftheworld.txt','w') as hellos:
+    hellos.write('Ni hao, shijie \n')
+    hellos.write('Hola mundo \n')
+    hellos.write('Konnichiwa sekai \n')
